c97fdbf840eb6b0da58f24cae257cf8ce7067ca9
David Alvarez <marava.jr@gmail.com>
2022-07-13 17:17:38 +0200

    conectado con mongodb

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+require('dotenv').config()
+require('./mongo')
+
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+const Person = require('./models/Person')
 
 morgan.token('post-data', (req, res) => {
   if (req.method === 'POST') {
@@ -24,30 +28,32 @@ app.use(morgan((tokens, req, res) => {
 app.use(express.static('build'))
 
 let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
+  // { 
+  //   "id": 1,
+  //   "name": "Arto Hellas", 
+  //   "number": "040-123456"
+  // },
+  // { 
+  //   "id": 2,
+  //   "name": "Ada Lovelace", 
+  //   "number": "39-44-5323523"
+  // },
+  // { 
+  //   "id": 3,
+  //   "name": "Dan Abramov", 
+  //   "number": "12-43-234345"
+  // },
+  // { 
+  //   "id": 4,
+  //   "name": "Mary Poppendieck", 
+  //   "number": "39-23-6423122"
+  // }
 ]
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/info', (request, response) => {
@@ -56,21 +62,23 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).json({
-      error: "Not found"
-    })
-  }
+  const { id } = request.params
+  Person.findById(id).then(person => {
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).json({
+        error: "Not found"
+      })
+    }
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-  response.status(204).end()
+  const { id } = request.params
+  Person.findByIdAndDelete(id).then(() => {
+    response.status(204).end()
+  })
 })
 
 app.post('/api/persons', (request, response) => {
@@ -85,16 +93,16 @@ app.post('/api/persons', (request, response) => {
       error: 'name must be unique'
     })
   }
-  const newPerson = {
-    id: Math.floor(Math.random() * Math.pow(10,20)),
+  const newPerson = new Person({
     name: person.name,
     number: person.number
-  }
-  persons = [...persons, newPerson]
-  response.status(201).json(newPerson)
+  })
+  newPerson.save().then(savedPerson => {
+    response.status(201).json(savedPerson)
+  })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
---
+++ mongo.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+
+const connectionString = process.env.MONGO_DB_URI
+
+mongoose.connect(connectionString).then(() => {
+  console.log('Connected to Database')
+}).catch(err => console.log(err))
